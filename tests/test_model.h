9eda0100a2a8d8a9083769b41eeb5c985876d23a
Rodrigo Tobar <rtobar@icrar.org>
2016-09-01 18:27:28 +0800

    test: first cut
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ tests/test_model.h
@@ -0,0 +1,89 @@
+/**
+ * model tests
+ *
+ * ICRAR - International Centre for Radio Astronomy Research
+ * (c) UWA - The University of Western Australia, 2016
+ * Copyright by UWA (in the framework of the ICRAR)
+ * All rights reserved
+ *
+ * Contributed by Rodrigo Tobar
+ *
+ * This file is part of libprofit.
+ *
+ * libprofit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libprofit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libprofit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cxxtest/TestSuite.h>
+
+#include "profit/profit.h"
+
+using namespace profit;
+
+class TestPsf : public CxxTest::TestSuite {
+
+public:
+
+	void test_valid_dimensions(void) {
+
+		Model m;
+
+		m.width = 0;
+		TS_ASSERT_THROWS(m.evaluate(), const invalid_parameter &);
+		m.height = 0;
+		TS_ASSERT_THROWS(m.evaluate(), const invalid_parameter &);
+
+		m.width = 1;
+		m.height = 1;
+		m.evaluate(); // fine...
+
+	}
+
+	void test_valid_scales(void) {
+
+		Model m;
+		m.width = 1;
+		m.height = 1;
+
+		m.scale_y = 1;
+		for(auto scale: {-2, -1, 0}){
+			m.scale_x = scale;
+			TS_ASSERT_THROWS(m.evaluate(), const invalid_parameter &);
+		}
+		m.scale_x = 0.1;
+		m.evaluate(); // fine
+
+		for(auto scale: {-2, -1, 0}){
+			m.scale_y = scale;
+			TS_ASSERT_THROWS(m.evaluate(), const invalid_parameter &);
+		}
+		m.scale_y = 0.1;
+		m.evaluate(); // fine
+
+	}
+
+	void test_valid_profiles(void) {
+
+		Model m;
+		for(auto name: {"unknown", "sersic1", "Sersic", "sersi", " sersic", "sersic "}) {
+			TS_ASSERT_THROWS(m.add_profile(name), const invalid_parameter &);
+		}
+
+		// these are fine
+		for(auto name: {"coresersic", "ferrer", "king", "moffat", "psf", "sersic", "sky"}) {
+			m.add_profile(name);
+		}
+
+	}
+
+};
---
+++ tests/test_psf.h
@@ -0,0 +1,62 @@
+/**
+ * psf profile tests
+ *
+ * ICRAR - International Centre for Radio Astronomy Research
+ * (c) UWA - The University of Western Australia, 2016
+ * Copyright by UWA (in the framework of the ICRAR)
+ * All rights reserved
+ *
+ * Contributed by Rodrigo Tobar
+ *
+ * This file is part of libprofit.
+ *
+ * libprofit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libprofit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libprofit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <cxxtest/TestSuite.h>
+
+#include "profit/psf.h"
+
+using namespace profit;
+
+class TestPsf : public CxxTest::TestSuite {
+
+public:
+
+	void test_exact_pixels(void) {
+
+		Model m;
+		m.width = 10;
+		m.height = 10;
+		m.psf_width = 2;
+		m.psf_height = 2;
+		m.magzero = 0;
+		m.psf = {1,1,1,1};
+
+		auto psfprof = static_cast<PsfProfile &>(m.add_profile("psf"));
+		psfprof.xcen = 2;
+		psfprof.ycen = 2;
+		psfprof.mag = 0;
+
+		std::vector<double> image = m.evaluate();
+		for(auto j=0; j!=2; j++) {
+			for(auto i=0; i!=2; i++) {
+				TS_ASSERT_DELTA(0.25, image[i + j*m.width], 1e9);
+			}
+		}
+
+	}
+
+};
