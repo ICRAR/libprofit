38ff268576e40c5514cd3054b2fb9f4e892cc93f
Rodrigo Tobar <rtobar@icrar.org>
2018-10-11 17:47:13 +0800

    Adding tests for all supported instruction sets
    This should increase our code coverage, making sure we don't break most
    of the supported SIMD instruction sets (including NONE) in the future.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- tests/test_convolver.h
+++ tests/test_convolver.h
@@ -151,7 +151,19 @@ private:
 public:
 
 	void test_new_bruteforce_convolver() {
-		_check_convolver(create_convolver(ConvolverType::BRUTE, ConvolverCreationPreferences()));
+		ConvolverCreationPreferences prefs;
+		_check_convolver(create_convolver(ConvolverType::BRUTE, prefs));
+		for (auto instruction_set: {simd_instruction_set::AVX, simd_instruction_set::SSE2, simd_instruction_set::NONE, simd_instruction_set::AUTO}) {
+			std::ostringstream msg;
+			msg << instruction_set << " should be invalid";
+			prefs.instruction_set = instruction_set;
+			if (has_simd_instruction_set(instruction_set)) {
+				_check_convolver(create_convolver(ConvolverType::BRUTE, prefs));
+			}
+			else {
+				TSM_ASSERT_THROWS(msg.str(), create_convolver(ConvolverType::BRUTE, prefs), const invalid_parameter &);
+			}
+		}
 	}
 
 	void test_old_bruteforce_openmp() {
