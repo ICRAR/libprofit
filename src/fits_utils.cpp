ICRAR/libprofit

commit b44c64217a30529cc6b9a8af3ddbd533db9da48e

Avoiding memory allocation on FITS dumping
We can avoid a potentially big and expensive memory allocation when
doing the conversion from little to big ending needed for FITS data
dumping. Instead, we loop through the pixels, convert them individually,
and write them into the file immediately after conversion. OS caching
saves us from having a bad writing performance most probably, and anyway
it's not really an issue for us as the tool is mostly exploratory and is
not meant to be *that* performant.

Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

src/fits_utils.cpp
@@ -190,10 +190,11 @@ void to_fits(const Image &image, const Point &offset, const PixelScale &pixel_sc
 
 	/* data has to be big-endian */
 	size_t image_size = image.size();
-	if( is_little_endian() ) {
-		std::vector<double> big_endian_image(image_size);
-		std::transform(image.begin(), image.end(), big_endian_image.begin(), swap_bytes);
-		f.write(reinterpret_cast<const char *>(big_endian_image.data()), sizeof(double) * image_size);
+	if (is_little_endian()) {
+		std::for_each(image.begin(), image.end(), [&f](double pixel) {
+			swap_bytes(pixel);
+			f.write(reinterpret_cast<const char *>(&pixel), sizeof(double));
+		});
 	}
 	else {
 		f.write(reinterpret_cast<const char *>(image.data()), sizeof(double) * image_size);

commit cc4c1ef6454203c4c45014bb37b991130b71aa73

Writing big-endian data, not little endian
This was a huge oversight introduced in b44c642 which we failed to
recognize, but it was very clear once one tried to open images
containing little-endian data.

Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

src/fits_utils.cpp
@@ -192,7 +192,7 @@ void to_fits(const Image &image, const Point &offset, const PixelScale &pixel_sc
 	size_t image_size = image.size();
 	if (is_little_endian()) {
 		std::for_each(image.begin(), image.end(), [&f](double pixel) {
-			swap_bytes(pixel);
+			pixel = swap_bytes(pixel);
 			f.write(reinterpret_cast<const char *>(&pixel), sizeof(double));
 		});
 	}